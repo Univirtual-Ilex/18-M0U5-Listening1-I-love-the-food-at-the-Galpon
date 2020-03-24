2f8b39368548bd097629ea144cceaa6d8bda299d
Alex Agudelo <agara85@gmail.com>
2020-03-24 16:55:54 -0500

    ready to production

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home_base = function ({ staticContext, ...props}) { // se le pasan las pro
     return (
         <Container {...props}  row bgImage="src/bg.png">
 
-                    <div className='bloque-izquierdo'>
+                    <div className='bloque-izquierdo entrada-top-Y-2'>
                         <div className='bloque-izquierdo-container'>
                             <h1> ¡ I love the food at the Galpon! </h1>
                             <p>
@@ -20,7 +20,7 @@ const Home_base = function ({ staticContext, ...props}) { // se le pasan las pro
                             <ButtonLink to='/actividad1'>Start</ButtonLink>
                         </div>
                     </div>
-                    <div className='bloque-derecho'>
+                    <div className='bloque-derecho entrada-top-Y'>
                         <ICol mt={2}>
                             <figure>
                                 <img src='./src/cover.png' alt='Logo' />    
